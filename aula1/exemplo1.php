b9dee29d690cafb44095c1d3565d1503e1c65fa2
Camila Betania Barbosa de Morais <14884124642@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-02-16 10:40:12 -0300

    Carga Inicial

---
+++ aula1/exemplo1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for($c = 1; $c <=50; $c = $c +1){
+    ?>
+    <img src="https://i.pinimg.com/736x/21/18/54/2118543235117ee07b6301814c774a33.jpg" 
+    alt="imagem gato fofo"
+    width="10%">
+    <?php } ?>
+</body>
+</html>
---
+++ aula2/operador_ari.php
@@ -0,0 +1,68 @@
+<h2>Operadores Aritméticos</h2>
+<hr>
+
+<p>
+    São utilizados para realizar cálculos matemáticos.
+</p>
+
+tr => table row => linha da tabela 
+td => tabela data => dados da tabela 
+
+<table border=1 width="100%">
+    <thead>
+    <tr>
+        <th>Operador</th>
+        <th>Descrição</th>
+        <th>Exemplo</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <tr>
+        <td>+</td>
+        <td>Soma</td>
+        <td>$Soma = 10 + 5;</td>
+    </tr>
+    <tr>
+        <td>-</td>
+        <td>Subtração</td>
+        <td>$sub = 10 - 5;</td>
+    </tr>
+    <tr>
+        <td>/</td>
+        <td>Divisão</td>
+        <td>$div = 10 / 5;</td>
+    </tr>
+    <tr>
+        <td>*</td>
+        <td>Multiplicação</td>
+        <td>$multi = 10 * 5;</td>
+    </tr>
+    <tr>
+        <td>%</td>
+        <td>Resto da divisão</td>
+        <td>$resto = 10 % 5;</td>
+    </tr>
+    <tr>
+        <td>**</td>
+        <td>Potênciação</td>
+        <td>$pot = 10 ** 5;</td>
+    </tr>
+    <tbody>
+</table>
+
+<?php
+$soma = 10 + 5;
+$sub = 10 - 5;
+$div = 10 / 5;
+$multi = 10 * 5;
+$resto = 10 % 5;
+$pot = 10 ** 5;
+
+echo "Soma $soma <br>";
+echo "Subtração $sub <br>";
+echo "Divisão $div <br>";
+echo "Multiplicação $multi <br>";
+echo "Resto da divisão $resto <br>";
+echo "Potenciação $pot <br>";
+?>
