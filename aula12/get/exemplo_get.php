fea041b56c2804f6be0a96b0813eae6fa1c779c9
Camila Betania Barbosa de Morais <14884124642@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-20 08:40:37 -0300

    aula12

---
+++ aula12/get/exemplo_get.php
@@ -0,0 +1,13 @@
+<?php
+    $resultado = $_GET['numero1'] + $_GET['numero2'];
+    echo "O resultado da soma é $resultado";
+
+//para fazer requisitos é usado $_GET(é usado para pegar arquivos) exemplo
+http://localhost/3infoA/Camila%20Bet%C3%A2nia/aula12/get/exemplo_get.php "?numero1=10&numero2=200"
+//o ? é usado para criar variavel e o & utiliza-se para adicionar mais variaveis
+
+//    para fazer a utilização do $_GET
+
+//    $resultado = $_GET["numero1"] + $_GET["numero2"];
+//echo "O resultado da some é $resultado"
+?>
---
+++ aula12/get/exercicio.php
@@ -0,0 +1,8 @@
+<?php
+
+for ($contador = 0; $contador <= 10; $contador++){
+    $resultado = $_GET['numero1'] * $contador;
+    echo $_GET['numero1']. "x $contador =   $resultado <br>";
+}
+
+?>
