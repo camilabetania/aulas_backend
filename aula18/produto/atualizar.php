5a8c19ebab13e5f9e8a2cc65f97f7851418b6e64
Camila Betania Barbosa de Morais <14884124642@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-05-18 10:33:44 -0300

    aula18

--- aula18/produto/atualizar.php
+++ aula18/produto/atualizar.php
@@ -4,11 +4,15 @@ require_once "../conexao.php";
 
 if(isset($_POST["nome"]) && isset($_POST["descricao"]) && isset($_POST["preco"]))
 {
+
+//inclui o arquivo para salvar a foto do upload
+require_once "salvar_foto.php";
+
 $id = $_POST["id"];
 $nome = $_POST["nome"];
 $descricao = $_POST["descricao"];
 $preco = $_POST["preco"];
-$foto = "semfoto.png";
+$foto = $nome_arquivo;
 
 //string com o comando slq para ser executado no db
 $sql = "UPDATE produto SET 
--- aula18/produto/inserir.php
+++ aula18/produto/inserir.php
@@ -4,10 +4,14 @@ require_once "../conexao.php";
 
 if(isset($_POST["nome"]) && isset($_POST["descricao"]) && isset($_POST["preco"]))
 {
+
+//inclui o arquivo para salvar a foto do upload
+require_once "salvar_foto.php";
+
 $nome = $_POST["nome"];
 $descricao = $_POST["descricao"];
 $preco = $_POST["preco"];
-$foto = "semfoto.png";
+$foto = $nome_arquivo;
 
 //string com o comando slq para ser executado no db
 $sql = "INSERT INTO `produto` (`nome`, `preco`, `descricao`, `foto`) 
