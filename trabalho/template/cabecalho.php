7924544805c700c52653a1275b6dec66cce1a220
Camila Betania Barbosa de Morais <14884124642@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-06-15 10:46:52 -0300

    aula19

--- trabalho/template/cabecalho.php
+++ trabalho/template/cabecalho.php
@@ -10,31 +10,4 @@
   </head>
   <body>
 
-
-    <!--carousel-->
-
- <div id="carouselExample" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="../imagens/imagem1.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="../imagens/imagem2.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="../imagens/ovo.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-
- <!--final do carousel-->
-
 </body>
